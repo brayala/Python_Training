ed29dff6d14d5432a2f83f7aaaac7e98983bb28d
brayala <76740789+brayala@users.noreply.github.com>
2022-04-19 11:19:27 -0600

    Add files via upload

---
+++ 1st Project - Band Name.py
@@ -0,0 +1,6 @@
+# Day 1, First Proyect - Band Name Generator
+
+print("Si no sabes que nombre ponerle a tu picó, aquí te ayudamos...")
+Cole_Name = input("Tírate el nombre del coleto del barrio..."'\n')
+Chawa_Name = input("Tírate una palabra en inglés, como power, remix, MC..."'\n')
+print("AQUIIIIIIII SUENAAAAAAA El " + Cole_Name + " " + Chawa_Name + " Stereo Laser !")
